7bbdc946fd80dcb3d69b872d7d6ba2fcc37de982
Fouche du Preez <fouche@limehouse.co.za>
2024-06-17 18:02:39 +0200

    wip JWT

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
@@ -29,4 +30,52 @@ type TokenPairs struct {
 
 type Claims struct {
 	jwt.RegisteredClaims
+}
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
+	// Create a token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set the claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	// below are mandatory claims
+	claims["sub"] = fmt.Sprint(user.ID) // subject
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix() // issued at
+	claims["typ"] = "JWT"
+
+	// Set the expiry for JWT
+	claims["exp"] = time.Now().UTC().Add(j.TokenExpiry).Unix()
+
+	// Create a signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// Create a refresh token and set claims
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+	refreshTokenClaims["iat"] = time.Now().UTC().Unix()
+
+	// Set expiry for the refresh token
+	refreshTokenClaims["exp"] = time.Now().UTC().Add(j.RefreshExpiry).Unix()
+
+	// Create signed refresh token
+	signedRefreshToken, err := refreshToken.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// Create TokenPairs and populate with signed tokens
+	var tokenPairs = TokenPairs{
+		Token:        signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	// Return TokenPairs
+	return tokenPairs, nil
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,27 +6,31 @@ import (
 	"github.com/fouched/go-movies-api/internal/repo"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = ":9080"
 
 type application struct {
-	DSN    string
-	Domain string
+	DSN          string
+	Domain       string
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
-func main() {
-	// set application config
-	var app application
+var app application
 
-	dbPool, err := initApp(app)
+func main() {
+	dbPool, err := initApp()
 	if err != nil {
 		log.Fatal(err)
 	}
 	// we have database connectivity, close it after app stops
 	defer dbPool.Close()
 
-	app.Domain = "example.com"
 	log.Println("Starting application on", port)
 	// start web server
 	err = http.ListenAndServe(port, app.routes())
@@ -35,13 +39,29 @@ func main() {
 	}
 }
 
-func initApp(app application) (*sql.DB, error) {
+func initApp() (*sql.DB, error) {
 	// read from command line, var to be populated, the flag, default value and some help text
 	flag.StringVar(&app.DSN,
 		"dsn",
 		"host=localhost port=5432 user=fouche password=javac dbname=movies sslmode=disable timezone=UTC connect_timeout=5",
 		"Database connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issues")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
 	flag.Parse()
+
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
 
 	dbPool, err := repo.CreateDbPool(app.DSN)
 	if err != nil {
