5890ffb0a52128576dab6c1bf781111c138f5b74
Fouche du Preez <fouche@limehouse.co.za>
2024-06-20 07:46:32 +0200

    wip JWT

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -5,7 +5,7 @@ import "net/http"
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://*")
 
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
